416893d6705a3a7f7167882148b0deef77c5a9a7
Kenneth Tay <kenneth21tay@gmail.com>
2025-07-22 16:41:13 +0800

    Setting up start up event for backend.

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+import tensorflow as tf
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	print("Loading model")
+	model = tf.keras.models.load_model("")
+	yield
+	print("Fastapi app ending")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def root():
