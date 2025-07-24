df0bfc1ff0363514adaf5ece7d39eeb0d930dbe6
Kenneth Tay <kenneth21tay@gmail.com>
2025-07-24 13:44:38 +0800

    Minor editing

--- frontend/food-vision-react/src/App.jsx
+++ frontend/food-vision-react/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header.jsx'
 import FoodBar from './components/FoodBar.jsx'
 import ImgInput from './components/ImgInput.jsx'
+import Result from './components/Result.jsx'
 
 function App() {
 	const [uploaded, setUploaded] = useState(false)
@@ -11,10 +12,13 @@ function App() {
 		<>
 			<Header />
 			<FoodBar />
-			<h2>This is a food classification model built with TensorFlow.</h2>
-			<h2>The uploaded image will be classified into one of the 101 classes of foods in this model</h2>
-			<h2><b>Note:</b>This model has an accuracy of xx.x% and its just meant to be a fun project that I made</h2>
+			<div className="introContainer">	
+				<h3>This is a food classification model build with TensorFlow.</h3>
+				<h3>The uploaded image will be classified into one of the 101 classes of foods in this model.</h3>
+				<h3><b>Note:</b> This model has an accuracy of xx.x% and its just meant to be a fun project that I made</h3>
+			</div>
 			<ImgInput />
+			<Result />
 		</>
 	)
 }
