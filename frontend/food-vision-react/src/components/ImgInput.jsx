cb20bf0b9a5febce16ce349bcb84d9e0e476c95f
Kenneth Tay <kenneth21tay@gmail.com>
2025-07-24 13:50:50 +0800

    Edit input html tag to only accept png and jpg.

--- frontend/food-vision-react/src/components/ImgInput.jsx
+++ frontend/food-vision-react/src/components/ImgInput.jsx
@@ -4,7 +4,7 @@ export default function ImgInput() {
     return (
         <div className="inputContainer">
             <h3>Upload an image file</h3>
-            <input type="file" accept='image/*'/>
+            <input type="file" accept='image/jpg, image/png'/>
         </div>
     )
 }
